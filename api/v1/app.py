361d9b9d5912c78fb2a39e26c473a08ddb871ffb
root <Caramonh@yahoo.com>
2023-07-24 15:54:13 -0700

    flask

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,14 +2,15 @@
 """
 Starts up a copy of a flask-app
 """
-from models import storage
 from flask import Flask
 from api.v1.views import app_views
 from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+
+#Allow CORS for all routes on 0.0.0.0
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
